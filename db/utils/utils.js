5bdc144a9159ab0a6aa886a932446eac449fbdca
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-06-24 21:03:49 +0100

    utils function completed

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,4 +1,10 @@
-exports.formatDate = list => {};
+exports.formatDate = list => {
+  const newArray = list.map(item => {
+    const { created_at, ...restOfContent } = item;
+    return { created_at: new Date(item.created_at), ...restOfContent };
+  });
+  return newArray;
+};
 
 exports.makeRefObj = list => {};
 
