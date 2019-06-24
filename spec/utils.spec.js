13ec51ebfb67c149d9280e56e32a48bc47d7047c
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-06-24 21:05:34 +0100

    utils spec for format data

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,8 +1,42 @@
-const { expect } = require('chai');
-const { formatDate, makeRefObj, formatComments } = require('../db/utils/utils');
+const { expect } = require("chai");
+const { formatDate, makeRefObj, formatComments } = require("../db/utils/utils");
 
-describe('formatDate', () => {});
+describe("formatDate", () => {
+  it("returns an empty object when given an empty array", () => {
+    const input = [];
+    const actual = formatDate(input);
+    const expected = [];
+    expect(actual).to.eql(expected);
+  });
+  it("returns a a new object with a formated date when given an array", () => {
+    const input = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+    const actual = formatDate(input);
+    const expected = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: new Date(1542284514171),
+        votes: 100
+      }
+    ];
+    expect(actual).to.eql(expected);
+  });
+  it('returns', () => {
+    
+  });
+});
 
-describe('makeRefObj', () => {});
+describe("makeRefObj", () => {});
 
-describe('formatComments', () => {});
+describe("formatComments", () => {});
