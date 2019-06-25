11382ac0ffad20500c492ffc812336f0b57759a3
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-06-25 12:13:44 +0100

    utility function completed for formatComments

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -8,11 +8,33 @@ exports.formatDate = list => {
 
 exports.makeRefObj = (list, paramOne, paramTwo) => {
   const result = {};
-
   for (let i = 0; i < list.length; i++) {
     result[list[i][paramOne]] = list[i][paramTwo];
   }
   return result;
 };
 
-exports.formatComments = (comments, articleRef) => {};
+exports.renameKeys = input => {
+  const newComments = input.map(item => {
+    const { belongs_to, created_by, ...restOfContents } = item;
+    return { article_id: belongs_to, author: created_by, ...restOfContents };
+  });
+
+  return newComments;
+};
+
+exports.formatComments = (comments, articleRef) => {
+  return comments.map(comment => {
+    const result = {};
+    Object.keys(comment).forEach(key => {
+      const value = comment[key];
+      if (key === "belongs_to") {
+        result["article_id"] = articleRef[comment.belongs_to];
+      } else {
+        result[key] = value;
+      }
+    });
+    console.log(result);
+    return result;
+  });
+};
