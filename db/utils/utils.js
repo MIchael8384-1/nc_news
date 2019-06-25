e8373b0f761acb4e86c3880acabe76f8c10f005c
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-06-25 10:58:50 +0100

    makeRefObj working

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -6,6 +6,13 @@ exports.formatDate = list => {
   return newArray;
 };
 
-exports.makeRefObj = list => {};
+exports.makeRefObj = (list, paramOne, paramTwo) => {
+  const result = {};
+
+  for (let i = 0; i < list.length; i++) {
+    result[list[i][paramOne]] = list[i][paramTwo];
+  }
+  return result;
+};
 
 exports.formatComments = (comments, articleRef) => {};
