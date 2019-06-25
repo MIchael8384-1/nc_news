f9e6bc88e86797ff392631467d3879b9159f5784
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-06-25 10:57:46 +0100

    test completed for the makeRefObj function

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -100,6 +100,58 @@ describe("formatDate", () => {
   });
 });
 
-describe("makeRefObj", () => {});
+describe.only("makeRefObj", () => {
+  it("return an empty object when given an array", () => {
+    const input = [];
+    const actual = makeRefObj(input);
+    const expected = {};
+    expect(actual).to.eql(expected);
+  });
+  it("returns an object with new key value pair when given a array with a single object ", () => {
+    const input = [
+      {
+        article_id: 1,
+        title: "Running a Node App",
+        topic: "coding",
+        author: "jessjelly",
+        body:
+          "This is part two of a series on how to get up and running with Systemd and Node.js. This part dives deeper into how to successfully run your app with systemd long-term, and how to set it up in a production environment.",
+        created_at: 1471522072389
+      }
+    ];
+    const actual = makeRefObj(input, "title", "article_id");
+    const expected = { "Running a Node App": 1 };
+    expect(actual).to.eql(expected);
+  });
+  it("returns an object with new key value pairs when given an array with multiple objects", () => {
+    const input = [
+      {
+        article_id: 1,
+        title: "Running a Node App",
+        topic: "coding",
+        author: "jessjelly",
+        body:
+          "This is part two of a series on how to get up and running with Systemd and Node.js. This part dives deeper into how to successfully run your app with systemd long-term, and how to set it up in a production environment.",
+        created_at: 1471522072389
+      },
+      {
+        article_id: 2,
+        title:
+          "The Rise Of Thinking Machines: How IBM's Watson Takes On The World",
+        topic: "coding",
+        author: "jessjelly",
+        body:
+          "Many people know Watson as the IBM-developed cognitive super computer that won the Jeopardy! gameshow in 2011. In truth, Watson is not actually a computer but a set of algorithms and APIs, and since winning TV fame (and a $1 million prize) IBM has put it to use tackling tough problems in every industry from healthcare to finance. Most recently, IBM has announced several new partnerships which aim to take things even further, and put its cognitive capabilities to use solving a whole new range of problems around the world.",
+        created_at: 1500584273256
+      }
+    ];
+    const actual = makeRefObj(input, "title", "article_id");
+    const expected = {
+      "Running a Node App": 1,
+      "The Rise Of Thinking Machines: How IBM's Watson Takes On The World": 2
+    };
+    expect(actual).to.eql(expected);
+  });
+});
 
 describe("formatComments", () => {});
