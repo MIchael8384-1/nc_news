f6b2d65ca84c995f2d524c7f53a475ea9a17bedf
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-06-27 15:26:59 +0100

    articles controller set up

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,7 +1,16 @@
 const {
+  fetchArticles,
   fetchArticleById,
   fetchArticleComments
 } = require("../models/articles");
+
+exports.getArticles = (req, res, next) => {
+  fetchArticles()
+    .then(articles => {
+      res.status(200).send({ articles });
+    })
+    .catch(next);
+};
 
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
