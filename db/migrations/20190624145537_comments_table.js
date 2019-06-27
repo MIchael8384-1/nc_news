a2e51f81e4a32bb5f0d8819f35039c86daffe738
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-06-27 10:50:41 +0100

    update arror handling for comments

--- db/migrations/20190624145537_comments_table.js
+++ db/migrations/20190624145537_comments_table.js
@@ -1,11 +1,17 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("comments", commentsTable => {
     commentsTable.increments("comment_id").primary();
-    commentsTable.string("author").references("users.username");
-    commentsTable.integer("article_id").references("articles.article_id");
+    commentsTable
+      .string("author")
+      .references("users.username")
+      .notNullable();
+    commentsTable
+      .integer("article_id")
+      .references("articles.article_id")
+      .notNullable();
     commentsTable.integer("votes").defaultTo(0);
     commentsTable.string("created_at").defaultTo(new Date());
-    commentsTable.string("body", 2000);
+    commentsTable.string("body", 2000).notNullable();
   });
 };
 
