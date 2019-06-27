f222f5728f0bf7bd9dbe9042fe8f388c616d8f77
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-06-27 15:25:10 +0100

    getArticles route set up

--- routes/articles.js
+++ routes/articles.js
@@ -1,10 +1,11 @@
 const articlesRouter = require("express").Router();
 const {
+  getArticles,
   getArticleById,
   getArticleComments
 } = require("../controllers/articles");
 
-//articlesRouter.route("/").get(getArticleById);
+articlesRouter.route("/").get(getArticles);
 articlesRouter.route("/:article_id").get(getArticleById);
 articlesRouter.route("/:article_id/comments").get(getArticleComments);
 
