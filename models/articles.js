34903a69b1bb739cb88579897af28a675bbca16c
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-06-27 15:28:41 +0100

    fetch articles add to my model

--- models/articles.js
+++ models/articles.js
@@ -1,5 +1,23 @@
 const connection = require("../db/connection");
 
+exports.fetchArticles = (sort_by = "created_at", order = "DESC") => {
+  return connection
+    .select(
+      "articles.author",
+      "articles.title",
+      "articles.article_id",
+      "articles.topic",
+      "articles.created_at",
+      "articles.votes"
+    )
+    .from("articles")
+    .count({ comments_count: "comment_id" })
+    .leftJoin("comments", "articles.article_id", "comments.article_id")
+    .groupBy("articles.article_id")
+    .orderBy(sort_by, order);
+  //.modify((query) => {});
+};
+
 exports.fetchArticleById = article_id => {
   return connection
     .select("articles.*")
@@ -21,10 +39,3 @@ exports.fetchArticleComments = (
     .where({ article_id })
     .orderBy(sort_by, order);
 };
-
-/*
-  SELECT comment_id, comments.votes, comments.created_at, comments.author, comments.body
-  FROM articles
-  LEFT JOIN comments
-  ON articles.article_id = comments.article_id
-  */
