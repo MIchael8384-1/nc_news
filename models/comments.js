2b09076318cac933f16aeaee9f9cfbc7c08a1388
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-06-27 10:52:27 +0100

    set up querys on the model

--- models/comments.js
+++ models/comments.js
@@ -4,5 +4,14 @@ exports.removeCommentById = comment_id => {
   return connection
     .delete()
     .from("comments")
-    .where({ comment_id });
+    .where({ comment_id })
+    .then(deleteCount => {
+      if (!deleteCount) {
+        return Promise.reject({
+          status: 404,
+          msg: `Comment_Id ${comment_id} can not be found`
+        });
+      }
+      return true;
+    });
 };
