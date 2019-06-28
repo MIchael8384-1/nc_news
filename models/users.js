22da855f83784bdee70561415173c47a69cb37be
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-06-28 10:30:40 +0100

    users model updated

--- models/users.js
+++ models/users.js
@@ -5,5 +5,14 @@ exports.fetchUserByUsername = username => {
   return connection
     .select("username", "name", "avatar_url")
     .from("users")
-    .where({ "users.username": username });
+    .where({ "users.username": username })
+    .then(([user]) => {
+      console.log(user);
+      if (!user)
+        return Promise.reject({
+          status: 404,
+          msg: "Username not found"
+        });
+      return user;
+    });
 };
