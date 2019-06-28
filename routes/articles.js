9cb0cd6e3b51c4ef2e55b7fe93f34c43174930b3
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-06-28 10:31:27 +0100

    article route updated

--- routes/articles.js
+++ routes/articles.js
@@ -7,6 +7,11 @@ const {
 
 articlesRouter.route("/").get(getArticles);
 articlesRouter.route("/:article_id").get(getArticleById);
-articlesRouter.route("/:article_id/comments").get(getArticleComments);
+articlesRouter
+  .route("/:article_id/comments")
+  .get(getArticleComments)
+  .all((req, res) => {
+    res.status(405).send({ msg: "Method is not allowed" });
+  });
 
 module.exports = articlesRouter;
