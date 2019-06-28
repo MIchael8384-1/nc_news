2a92442fd72a4e5351e4fd960b98ea2166eb2698
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-06-28 10:29:06 +0100

    user controller updated

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,12 @@
-const { fetchUserByUsername } = require("../models/users");
+const { fetchUserByUsername, checkExists } = require("../models/users");
 
 exports.getUserByUsername = (req, res, next) => {
   const { username } = req.params;
   fetchUserByUsername(username)
     .then(user => {
+      //console.log(user);
       res.status(200).send({ user });
     })
     .catch(next);
 };
+//res.status(200).send({ user });
