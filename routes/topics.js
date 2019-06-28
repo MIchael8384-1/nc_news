1c89cea3b02186af9da6fca0c265f1305a8eaa68
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-06-28 12:33:24 +0100

    topics method not allowed

--- routes/topics.js
+++ routes/topics.js
@@ -1,6 +1,11 @@
 const topicsRouter = require("express").Router();
 const { getTopics } = require("../controllers/topics");
 
-topicsRouter.route("/").get(getTopics);
+topicsRouter
+  .route("/")
+  .get(getTopics)
+  .all((req, res) => {
+    res.status(405).send({ msg: "Method is not allowed" });
+  });
 
 module.exports = topicsRouter;
