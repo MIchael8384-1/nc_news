7dd0ab03e35568b8b2320cd94d4ce3e30cbd2e80
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-06-28 12:10:46 +0100

    error hanadling for routes

--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,9 @@ const commentsRouter = require("./comments");
 // apiRouter.use("/", (req, res, next) => {
 //   console.log("api Router");
 // });
-
+apiRouter.route("/").all((req, res) => {
+  res.status(405).send({ msg: "Method is not allowed" });
+});
 apiRouter.use("/topics", topicsRouter);
 apiRouter.use("/users", usersRouter);
 apiRouter.use("/articles", articlesRouter);
